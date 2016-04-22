97f974cd2a7c4d65d8dd52c2db248f60b5b46ac2
Grzegorz Piwowarek <gpw@touk.pl>
2016-04-22 17:43:18 +0200

    Add assert input file exists

--- pyramids-solver/src/Main.hs
+++ pyramids-solver/src/Main.hs
@@ -1,7 +1,13 @@
 import System.Environment (getArgs)
+import System.Directory (doesFileExist)
+import Control.Monad
 
 main = do
-  arg <- getArgs
-  print $ arg
+  putStrLn "Input file: "
+  fileName <- getLine
+  putStrLn ("Received file name: " ++ fileName)
+
+  fileExists <- doesFileExist fileName
+  unless fileExists $ error "File does not exist."
 
 
