acdf20ad28c1030479edf75226cabe66b289a5ed
Grzegorz Piwowarek <gpw@touk.pl>
2016-04-24 09:41:59 +0200

    Refactor Solver

--- pyramids-solver/src/Solver.hs
+++ pyramids-solver/src/Solver.hs
@@ -4,4 +4,4 @@ module Solver where
 data Pyramids = Pyramids [Maybe Int] [Maybe Int] [Maybe Int] [Maybe Int] deriving (Show, Read)
 
 solve :: Pyramids -> [[Int]]
-solve a = error "Not yet implemented" {-TODO-}
+solve _ = error "Not yet implemented" {-TODO-}
