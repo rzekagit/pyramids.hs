ecf974e9bafbdbdbf3e0e59683fcd30bf4c69d6d
Grzegorz Piwowarek <gpw@touk.pl>
2016-04-30 18:50:17 +0200

    Refactor test cases

--- src/SolverSpec.hs
+++ src/SolverSpec.hs
@@ -1,10 +1,18 @@
 module Main where
 
-import Test.QuickCheck
+import Test.QuickCheck (quickCheckResult)
+import Test.QuickCheck.Test (isSuccess)
+
+import System.Exit (exitFailure)
+import Control.Monad (when)
 import Solver
-import System.Exit
 
+prop_shouldSolve :: Bool
+prop_shouldSolve = solve (Pyramids [Just 3, Nothing, Just 1, Nothing] [Nothing, Nothing, Nothing, Nothing] [Nothing, Nothing, Just 4, Nothing] [Nothing, Just 3, Nothing, Nothing]) == [[2, 1, 4, 3], [3, 4, 2, 1], [1, 2, 3, 4], [4, 3, 2, 1]]
+
+main :: IO ()
 main = do
-    let givenTask = Pyramids [Just 3, Nothing, Just 1, Nothing] [Nothing, Nothing, Nothing, Nothing] [Nothing, Nothing, Just 4, Nothing] [Nothing, Just 3, Nothing, Nothing]
+  let tests = [ quickCheckResult prop_shouldSolve ]
 
-    if solve givenTask == [[2, 1, 4, 3], [3, 4, 2, 1], [1, 2, 3, 4], [4, 3, 2, 1]] then exitSuccess else exitFailure
+  success <- fmap (all isSuccess) . sequence $ tests
+  when (not success) $ exitFailure
