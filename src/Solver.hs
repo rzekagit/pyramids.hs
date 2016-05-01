9d0125b292fdb51b6a425e755896cf26f7337314
Grzegorz Piwowarek <gpw@touk.pl>
2016-05-01 09:23:43 +0200

    Add partial implementation

--- src/Solver.hs
+++ src/Solver.hs
@@ -3,5 +3,12 @@ module Solver where
 
 data Pyramids = Pyramids [Maybe Int] [Maybe Int] [Maybe Int] [Maybe Int] deriving (Show, Read)
 
+equalLengths :: [[a]] -> Bool
+equalLengths []                                       = True
+equalLengths (x:xs) | (length x == length (head xs) ) = equalLengths xs
+                    | otherwise                       = False
+
 solve :: Pyramids -> [[Int]]
-solve _ = error "Not yet implemented" {-TODO-}
+solve ( Pyramids [] _ _ _ )                            = []
+solve ( Pyramids a b c d ) | equalLengths [a, b, c, d] = [] {-TODO-}
+                           | otherwise                 = error "Invalid input"
