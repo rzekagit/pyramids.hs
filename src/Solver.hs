b7565c3f7d2eca490f9bfc90bcc92cff8f4698d6
Grzegorz Piwowarek <gpw@touk.pl>
2016-05-01 09:53:12 +0200

    Refactor Solver.hs

--- src/Solver.hs
+++ src/Solver.hs
@@ -4,12 +4,13 @@ module Solver where
 data Pyramids = Pyramids [Maybe Int] [Maybe Int] [Maybe Int] [Maybe Int] deriving (Show, Read)
 
 equalLengths :: [[a]] -> Bool
-equalLengths []                                       = True
-equalLengths (x:xs) | (length x == length (head xs) ) = equalLengths xs
-                    | otherwise                       = False
+equalLengths []                         = True
+equalLengths (x:xs) | firstTwoEqual     = equalLengths xs
+                    | otherwise         = False
+                    where firstTwoEqual = (length x == length (head xs) )
 
 solve :: Pyramids -> [[Int]]
-solve ( Pyramids [] _ _ _ )                            = []
-solve ( Pyramids a b c d ) | allEqual                  = [] {-TODO-}
-                           | otherwise                 = error "Invalid input"
+solve ( Pyramids [] _ _ _ )             = []
+solve ( Pyramids a b c d ) | allEqual   = [] {-TODO-}
+                           | otherwise  = error "Invalid input"
                            where allEqual = equalLengths [a, b, c, d]
